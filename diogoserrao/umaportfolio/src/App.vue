a9597bc4c0a80964cb3f660487fc0ceb0387ac64
diogoserrao <diogorabimserrao@gmail.com>
2024-01-15 23:39:20 +0000

    mobile

--- diogoserrao/umaportfolio/src/App.vue
+++ diogoserrao/umaportfolio/src/App.vue
@@ -1,13 +1,126 @@
-<script setup>
-import { RouterView } from "vue-router"
-
-</script>
-
 <template>
-  <RouterView></RouterView>
-  <NotFound></NotFound>
+  <div
+    :class="theme === 'light' ? 'light-theme' : 'dark-theme'"
+    class="main-body"
+  >
+    <div class="conteudo">
+      <div class="cabecalho">
+        <div class="esquerda">
+          <a href="/"><img class="foto" src="/foto.jpg" /></a>
+          © DH Smit
+        </div>
+        <div class="direita">
+          <div class="menu">
+            <div class="item ativo">about</div>
+            <div class="item">work</div>
+            <div class="item">solutions</div>
+          </div>
+          <div class="icons">
+            <button @click="changeTheme" class="icon lua">
+              <font-awesome-icon icon="fa-solid fa-moon" />
+            </button>
+            <button @click="changeTheme" class="icon sol">
+              <font-awesome-icon icon="fa-solid fa-sun" />
+            </button>
+          </div>
+        </div>
+      </div>
+      <RouterView></RouterView>
+    </div>
+    <Partepreta></Partepreta>
+  </div>
 </template>
 
-<style scoped>
+<script setup>
+import {ref} from "vue"
+import Partepreta from "./components/Partepreta.vue";
+import { RouterView } from "vue-router";
 
+let theme = ref("light");
+
+const changeTheme = ()=> {
+  console.log("I am changing theme");
+  theme.value = theme.value === "dark" ? "light" : "dark";
+}
+</script>
+
+<style>
+.conteudo {
+  max-width: 1000px;
+  margin: 0 auto;
+  padding: 0 10px;
+}
+.light-theme {
+  background-color: white;
+  color: black;
+}
+.dark-theme {
+  background-color: black;
+  color: white;
+}
+.cabecalho {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 40px 0px;
+}
+.esquerda {
+  display: flex;
+  align-items: center;
+  gap: 20px;
+}
+.foto {
+  border-radius: 50%;
+  height: 200px;
+  width: 200px;
+  object-fit: none;
+}
+.direita {
+  display: flex;
+  gap: 50px;
+}
+.menu {
+  border: 1px solid gray;
+  border-radius: 55px;
+  display: flex;
+  height: 62px;
+}
+.item {
+  padding: 20px;
+  font-size: 20px;
+  padding-left: 50px;
+  padding-right: 50px;
+}
+.ativo {
+  border-radius: 55px;
+  border-right: solid 1px grey;
+}
+.icons {
+  display: flex;
+  gap: 10px;
+}
+.icon {
+  border-radius: 50%;
+  border: 1px solid gray;
+  width: 62px;
+  height: 62px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.lua {
+  background-color: black;
+  color: white;
+}
+.sol {
+  background-color: white;
+  color: black;
+}
+@media only screen and (max-width: 760px) {
+.direita{
+  display: none;
+}
+
+
+}
 </style>
--- diogoserrao/umaportfolio/src/views/Home.vue
+++ diogoserrao/umaportfolio/src/views/Home.vue
@@ -1,23 +1,20 @@
 <script setup>
-import Cabecalho from '../components/Cabecalho.vue'
 import Abertura from '../components/Abertura.vue'
 import AberturaTexto from '../components/AberturaTexto.vue'
 import Imagens from '../components/Imagens.vue'
-import Partepreta from '../components/Partepreta.vue'
+
 
 </script>
 
 <template>
   <div class="conteudo">
-  <Cabecalho></Cabecalho>
+    
   <Abertura></Abertura>
   <AberturaTexto></AberturaTexto>
   <Imagens></Imagens>
   </div>
-  <Partepreta></Partepreta>
-  <div class="pag2">
-
-  </div>
+  
+  
 </template>
 
 <style scoped>
@@ -25,8 +22,5 @@ import Partepreta from '../components/Partepreta.vue'
   max-width: 1000px;
   margin: 0 auto
 }
-.pag2{
-  max-width: 1000px;
-  margin: 0 auto
-}
+
 </style>
